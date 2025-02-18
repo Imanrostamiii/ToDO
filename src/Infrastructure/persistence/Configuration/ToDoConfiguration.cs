2f80c1c72cb87e3e3b4ba5280aba917246b68abc
Imanrostami <imanrostami8219@gmail.com>
2025-02-18 16:28:18 +0330

    Write Configuration Entity

--- src/Infrastructure/persistence/Configuration/ToDoConfiguration.cs
+++ src/Infrastructure/persistence/Configuration/ToDoConfiguration.cs
@@ -1,6 +1,26 @@
-﻿namespace Infrastructure.persistence.Configuration;
+﻿using Domin.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class ToDoConfiguration:IEntityConfiguration
+namespace Infrastructure.persistence.Configuration;
+
+public class ToDoConfiguration:IEntityTypeConfiguration<ToDo>
 {
-    
+    public void Configure(EntityTypeBuilder<ToDo> builder)
+    {
+        builder.ToTable("ToDO","ToDo");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasMaxLength(300);
+        
+        builder.Property(x => x.Description)
+            .IsRequired(false)
+            .HasMaxLength(500);
+        
+        builder.Property(x => x.ImagPath)
+            .IsRequired(false)
+            .HasMaxLength(600);
+    }
 }
---
+++ src/Infrastructure/persistence/Configuration/ToDoItemConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domin.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.persistence.Configuration;
+
+public class ToDoItemConfiguration:IEntityTypeConfiguration<ToDoItem>
+{
+    public void Configure(EntityTypeBuilder<ToDoItem> builder)
+    {
+        builder.ToTable("ToDoItem", "ToDo");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasMaxLength(300);
+    }
+}
