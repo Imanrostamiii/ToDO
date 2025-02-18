969a49853994f84ff8cca1ccb0c9a8ad90bd4403
Imanrostami <imanrostami8219@gmail.com>
2025-02-18 16:48:03 +0330

    Creat Miagration

---
+++ src/Infrastructure/persistence/Miagration/20250218131730_Initial.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.persistence.Miagration
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ToDo");
+
+            migrationBuilder.CreateTable(
+                name: "ToDO",
+                schema: "ToDo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    ImagPath = table.Column<string>(type: "nvarchar(600)", maxLength: 600, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDO", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoItem",
+                schema: "ToDo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    ToDoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsComplete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDoItem_ToDO_ToDoId",
+                        column: x => x.ToDoId,
+                        principalSchema: "ToDo",
+                        principalTable: "ToDO",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoItem_ToDoId",
+                schema: "ToDo",
+                table: "ToDoItem",
+                column: "ToDoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoItem",
+                schema: "ToDo");
+
+            migrationBuilder.DropTable(
+                name: "ToDO",
+                schema: "ToDo");
+        }
+    }
+}
