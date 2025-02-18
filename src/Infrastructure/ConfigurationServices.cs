665870efa1193f77696e62ad2823181a6dd19978
Imanrostami <imanrostami8219@gmail.com>
2025-02-18 16:42:34 +0330

    Add DbContext

---
+++ src/Infrastructure/ConfigurationServices.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class ConfigurationServices
+{
+   public static IServiceCollection AddInfrastructure(this IServiceCollection service, IConfiguration configuration)
+   {
+      service.AddDbContext<ApplicationDbContext>(option =>
+         option.UseSqlServer(configuration.GetConnectionString("ToDoList")));
+      return service;
+   }
+}
---
+++ src/Infrastructure/persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Infrastructure.persistence;
+
+public class ApplicationDbContext:DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+        
+    }
+}
